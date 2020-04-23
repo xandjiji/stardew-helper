a1063b34e5aeafd0f3e84a240e0bad4f1eef9c0e
xandjiji <xandjiji@gmail.com>
2020-04-23 19:58:49 -0300

    item component prototype

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react'
 
 import '../css/item.css';
+import '../css/spritesheet.css';
 
 export class Item extends Component {
 
     render() {
         
         let itemData = this.props.item;
+        let itemClass = buildClassName(itemData.name);
+
+        let itemInfo = itemData.info;
+        if(itemInfo) {
+            itemInfo = <div className="info smooth">{itemData.info}</div>;
+        }
         
         return (
             <div className="item">
-                {itemData.name}
+                <div className={`sprite ${itemClass}`}></div>
+                <span>{itemData.name}</span>
+                
+                {itemInfo}
             </div>
         )
     }
 }
 
 export default Item
+
+function buildClassName(name) {
+    name = name.replace(/'/g, "");
+    name = name.replace(/,/g, "");
+    name = name.replace(/ /g, "_");
+
+    return `bg-${name}`;
+}
