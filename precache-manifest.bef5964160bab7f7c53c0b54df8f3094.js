3eda052502645e9f32af51ff47388599c39c6112
xandjiji <xandjiji@gmail.com>
2020-05-10 11:01:49 -0300

    Updates

---
+++ precache-manifest.bef5964160bab7f7c53c0b54df8f3094.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09caaa2de933bde8608641a9323e897",
+    "url": "/stardew-helper/index.html"
+  },
+  {
+    "revision": "73d5e07eb25bec5244b6",
+    "url": "/stardew-helper/static/css/main.8b5c4f6c.chunk.css"
+  },
+  {
+    "revision": "84ff55d7f611fd1a3c88",
+    "url": "/stardew-helper/static/js/2.13c43d5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stardew-helper/static/js/2.13c43d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d5e07eb25bec5244b6",
+    "url": "/stardew-helper/static/js/main.4549cf82.chunk.js"
+  },
+  {
+    "revision": "d4524e0bc18663a4b668",
+    "url": "/stardew-helper/static/js/runtime-main.f4c516d1.js"
+  },
+  {
+    "revision": "c74706ea86f9d361a59ccc4ada1a38a1",
+    "url": "/stardew-helper/static/media/css_sprites.c74706ea.png"
+  }
+]);
