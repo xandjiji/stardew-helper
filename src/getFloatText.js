db54295c22c68a2bb3511f9866c1807f90d07590
xandjiji <xandjiji@gmail.com>
2020-05-17 18:42:36 -0300

    floatText adjustments

--- src/getFloatText.js
+++ src/getFloatText.js
@@ -1,12 +1,12 @@
 const getFloatText = () => {
     let messages = [
-        'amazing!',
-        'great!',
-        'cool!',
-        'perfect!',
-        'nice!',
-        'noice!',
-        'wow!'
+        'amazing !',
+        'great !',
+        'cool !',
+        'perfect !',
+        'nice !',
+        'noice !',
+        'wow !'
     ];
 
     return messages[Math.floor(Math.random() * messages.length)];
